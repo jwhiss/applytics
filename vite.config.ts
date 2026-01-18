1fe52d0d6fdbb95caa1d1220c3739bef0d0760d6
jwhiss <JoelJWhissel@gmail.com>
2026-01-18 00:47:20 -0700

    move index.html to src/index.html

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: 'src',
+  publicDir: '../public',
   base: './',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
 })
